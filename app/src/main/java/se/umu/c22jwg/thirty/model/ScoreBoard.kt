b4fc104244b375cf14f049f028ba2f0f197c091c
Jonatan Westling <jonatanwestling4@gmail.com>
2025-06-24 15:58:27 +0200

    Feat: make scoreboard implement parcable so we can store it in the viewmodel

--- app/src/main/java/se/umu/c22jwg/thirty/model/ScoreBoard.kt
+++ app/src/main/java/se/umu/c22jwg/thirty/model/ScoreBoard.kt
@@ -1,4 +1,42 @@
 package se.umu.c22jwg.thirty.model
 
-class ScoreBoard {
+import android.os.Parcel
+import android.os.Parcelable
+class ScoreBoard() : Parcelable {
+    // List to store scores
+    private val scores: MutableList<Pair<String, Int>> = mutableListOf()
+
+    constructor(parcel: Parcel) : this() {
+        val size = parcel.readInt()
+        repeat(size) {
+            val choice = parcel.readString() ?: ""
+            val score = parcel.readInt()
+            scores.add(choice to score)
+        }
+    }
+
+    fun addScore(choice: String, score: Int) {
+        scores.add(choice to score)
+    }
+
+    fun getScores(): List<Pair<String, Int>> = scores.toList()
+
+    fun getScoreForRound(round: Int): Pair<String, Int> = scores[round]
+
+    fun calculateTotalScore(): Int = scores.sumOf { it.second }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(scores.size)
+        for ((choice, score) in scores) {
+            parcel.writeString(choice)
+            parcel.writeInt(score)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<ScoreBoard> {
+        override fun createFromParcel(parcel: Parcel): ScoreBoard = ScoreBoard(parcel)
+        override fun newArray(size: Int): Array<ScoreBoard?> = arrayOfNulls(size)
+    }
 }
