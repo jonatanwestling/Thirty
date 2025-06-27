df09a88615f9dad7ba70e297f564996e287b494d
Jonatan Westling <jonatanwestling4@gmail.com>
2025-06-27 11:19:17 +0200

    Feat: implemented Parcelable to save state

--- app/src/main/java/se/umu/c22jwg/thirty/model/Die.kt
+++ app/src/main/java/se/umu/c22jwg/thirty/model/Die.kt
@@ -1,10 +1,31 @@
 package se.umu.c22jwg.thirty.model
 
-class Die {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Die: Parcelable {
     var value: Int = 1;
     var selected = false;
 
     fun roll() {
         value = (1..6).random();
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(value)
+        dest.writeByte(if (selected) 1 else 0)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Die> {
+        override fun createFromParcel(parcel: Parcel): Die {
+            val die = Die()
+            die.value = parcel.readInt()
+            die.selected = parcel.readByte().toInt() != 0
+            return die
+        }
+
+        override fun newArray(size: Int): Array<Die?> = arrayOfNulls(size)
     }
 }
--- app/src/main/java/se/umu/c22jwg/thirty/model/DieSet.kt
+++ app/src/main/java/se/umu/c22jwg/thirty/model/DieSet.kt
@@ -1,9 +1,11 @@
 package se.umu.c22jwg.thirty.model
 import se.umu.c22jwg.thirty.model.Die
+import android.os.Parcel
+import android.os.Parcelable
 
-class DieSet {
+class DieSet() : Parcelable {
     // List of all the dice in the set
-    private val diceSet: List<Die> = List(6) { Die() }
+    private val diceSet: MutableList<Die> = MutableList(6) { Die() }
 
     // Get the list of dice in the set
     val getDiceSet: List<Die>
@@ -20,14 +22,7 @@ class DieSet {
             die.roll();
         }
     }
-    // Roll the selected dice in the set
-    fun rollSelectedDice() {
-        for (die in diceSet) {
-            if (die.selected) {
-                die.roll();
-            }
-        }
-    }
+
     // Roll the dice that are not selected in the set
     fun rollOtherDice() {
         for (die in diceSet) {
@@ -51,5 +46,24 @@ class DieSet {
     // Reset all selections to false
     fun resetSelection() {
         diceSet.forEach { it.selected = false }
+    }
+
+    constructor(parcel: Parcel) : this() {
+        val dice = mutableListOf<Die>()
+        parcel.readTypedList(dice, Die.CREATOR)
+        for (i in dice.indices) {
+            diceSet[i] = dice[i]
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeTypedList(diceSet)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<DieSet> {
+        override fun createFromParcel(parcel: Parcel): DieSet = DieSet(parcel)
+        override fun newArray(size: Int): Array<DieSet?> = arrayOfNulls(size)
     }
 }
