495b4300fb1cd50246e492c3b00508e669136e9c
Jonatan Westling <jonatanwestling4@gmail.com>
2025-06-28 00:22:18 +0200

    Fix: rename function and debug logs

--- app/src/main/java/se/umu/c22jwg/thirty/model/ScoreBoard.kt
+++ app/src/main/java/se/umu/c22jwg/thirty/model/ScoreBoard.kt
@@ -2,6 +2,8 @@ package se.umu.c22jwg.thirty.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
+
 class ScoreBoard() : Parcelable {
     // List to store scores
     private val scores: MutableList<Pair<String, Int>> = mutableListOf()
@@ -16,14 +18,18 @@ class ScoreBoard() : Parcelable {
     }
 
     fun addScore(choice: String, score: Int) {
+        Log.d("ScoreBoard", "Adding score for choice: $choice, score: $score")
         scores.add(choice to score)
     }
 
-    fun getScores(): List<Pair<String, Int>> = scores.toList()
+    fun getScores(): List<Pair<String, Int>> {
+        Log.d("ScoreBoard", "Getting scores: $scores")
+        return scores.toList()
+    }
 
     fun getScoreForRound(round: Int): Pair<String, Int> = scores[round]
 
-    fun calculateTotalScore(): Int = scores.sumOf { it.second }
+    fun getTotalScore(): Int = scores.sumOf { it.second }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(scores.size)
